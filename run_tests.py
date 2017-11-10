4013b838c88e26d8fe03ad0d97e65959475016fc
bunseokbot <bunseokbot@gmail.com>
2017-11-11 00:54:01 +0900

    Add unittest runner

---
+++ run_tests.py
@@ -0,0 +1,9 @@
+"""Testing based on unittest code."""
+import sys
+import unittest
+
+if __name__ == '__main__':
+    test_suite = unittest.TestLoader().discover('tests', pattern='*.py')
+    test_results = unittest.TextTestRunner(verbosity=2).run(test_suite)
+    if not test_results.wasSuccessful():
+        sys.exit(1)
